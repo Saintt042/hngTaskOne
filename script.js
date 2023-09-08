26c7e66998c6baaf343cd0a4c36a46ca5dbae131
Alinwachukwu Michael <saintmichaell42@gmail.com>
2023-09-08 13:14:06 +0100

    made date more readable

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDayOfTheWeek = daysOfWeek[new Date().getDay()];
     const currentUTCTime = new Date().getTime();
     const myTrack = "Front-End";
-    const githubURL = "https://github.com/your-username/your-repository";
+    const githubURL = "https://github.com/Saintt042/hngTaskOne";
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Format the current date as a locale-specific date string
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const currentDayOfTheWeek = currentDate.toLocaleDateString(undefined, options);
+
+    
 
     // Update the DOM with the data
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDayOfTheWeek;
